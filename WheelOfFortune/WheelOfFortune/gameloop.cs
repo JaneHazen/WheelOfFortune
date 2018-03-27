3865a7421b8515e549b5c22b15db521a3a8fabc5
Sereina Blanchard (InConsulting Inc.) <v-seblan@microsoft.com>
2018-03-27 16:21:55 -0700

    added check if char is guessed method, started display string for char count

--- WheelOfFortune/WheelOfFortune/gameloop.cs
+++ WheelOfFortune/WheelOfFortune/gameloop.cs
@@ -1,9 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class GameLoop
 {
-	public GameLoop()
+
+    // Checks if character has already been guessed
+    private bool checkIfCharGuessed(string charGuess, List<string> previousGuesses)
+    {
+        var check = previousGuesses.Contains(charGuess);
+
+        return check;
+
+    }
+    public GameLoop(object gameData)
 	{
+        // PLAYER CHOOSES TO GUESS A LETTER
+        string playerCharGuess = Console.ReadLine();
+
+        int charCount = 0;
+        string turnResult;
+
+
+        // Check if letter guessed is in the answer
+        // If so, display how many of that character there are
+
+        // if char hasn't been guessed before
+        if(checkIfCharGuessed(playerCharGuess, gameData.previousGuesses))
+        {
+
+
+            // if char is in anser
+            if(charCount > 0)
+            {
+                // display "There are {charCount} guessChar in the answer
+                turnResult = $"There is/are {charCount} letter {playerCharGuess.ToUpper} in the puzzle";
+            }
+
+        }
+
+
+
+
 
 	}
 }
