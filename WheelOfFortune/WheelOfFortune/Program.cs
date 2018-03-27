6ed27754954bb5062a07668e4a3e0ad8f570aac5
Rhonda Smith <v-rhsmit@microsoft.com>
2018-03-27 16:12:53 -0700

    Starting winner stuff

--- WheelOfFortune/WheelOfFortune/Program.cs
+++ WheelOfFortune/WheelOfFortune/Program.cs
@@ -10,6 +10,7 @@ namespace WheelOfFortune
     {
         static void Main(string[] args)
         {
+            GameLoop();
         }
     }
 }
--- WheelOfFortune/WheelOfFortune/gameloop.cs
+++ WheelOfFortune/WheelOfFortune/gameloop.cs
@@ -4,6 +4,20 @@ public class GameLoop
 {
 	public GameLoop()
 	{
+        //User has pressed 1 to solve
+        //Take the input from the user (entire puzzle)
+        //Compare the user's answer to the actual answer
+        Console.WriteLine("Try to solve the puzzle");
+        var fullAnswerGuess = Console.ReadKey().ToString();
+        if(fullAnswerGuess == gameData.answer)
+        {
+            Console.WriteLine("hooray, you win!");
+        }
+        else
+        {
+            Console.WriteLine("try again!");
+            //Give user the choices again (0 quit, 1 solve, 2 spin);
+        }
 
 	}
 }
