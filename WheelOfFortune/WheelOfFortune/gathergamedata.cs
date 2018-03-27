dad20e58cbf914a8cf4500c50695204d2d59ae8b
v-aschin <v-aschin@MININT-19PV3Q4>
2018-03-27 16:17:29 -0700

    Created DisplayPlayerName method and added unit tests for respective code paths

--- WheelOfFortune/WheelOfFortune/gathergamedata.cs
+++ WheelOfFortune/WheelOfFortune/gathergamedata.cs
@@ -4,10 +4,33 @@ public class GatherGameData
 {
     public string PlayerName { get; set; }
 
+    /// <summary>
+    /// Asks the player for their name and reads it
+    /// </summary>
     public void GetPlayerName()
     {
         Console.WriteLine("Please enter your name:");
-        PlayerName = Console.ReadLine();
+        PlayerName = ParseInput(Console.ReadLine());
+    }
+
+    /// <summary>
+    /// For testing GetPlayerName method
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public string ParseInput(string input)
+    {
+        if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            throw new ArgumentNullException();
+        return input;
+    }
+
+    /// <summary>
+    /// Displays player name on the screen
+    /// </summary>
+    public void DisplayPlayerName()
+    {
+        Console.WriteLine($"Player Name : {PlayerName}");
     }
 
 }
