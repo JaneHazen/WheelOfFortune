dd7a2eaa3e939f9e0f44636efb1dde1d6e5fae1a
v-hara <sow.haby@gmail.com>
2018-03-27 18:07:57 -0700

    Added method to replace underscores with guessed word
    Related Work Items: #2176

--- WheelOfFortune/WheelOfFortune/gameloop.cs
+++ WheelOfFortune/WheelOfFortune/gameloop.cs
@@ -2,8 +2,30 @@
 
 public class GameLoop
 {
-	public GameLoop()
+	public GameLoop(object gameData)
 	{
 
 	}
+
+    public string ShowFoundLetters(string guessedChar, string answer, string underscoreTemplate)//pass in object to modify and word to find)
+    {  
+         var characterCount = 0;
+
+        // iterate through answer to see if char is included
+
+        for (int i = 0; i < answer; i++)
+            {
+            if (answer[i] == guessedChar)
+            {
+                characterCount++;
+                //replace letter 
+                underscoreTemplate[i] = guessedChar;
+                //remember to verify that guessedChar has upcase 
+            }  
+
+            }
+        return underscoreTemplate;
+
+    }
+
 }
